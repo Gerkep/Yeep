b8c672e0000319c8b50f1e73dfc566e0876c7836
Piotr Gerke <piotrg2003@gmail.com>
2022-10-10 11:20:42 +0200

    mantine core

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css'
 import '../styles/footer.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+  <MantineProvider withNormalizeCSS withGlobalStyles>
     <NotificationsProvider>
       <Component {...pageProps} />
     </NotificationsProvider>
+  </MantineProvider>
   )
 }
 
