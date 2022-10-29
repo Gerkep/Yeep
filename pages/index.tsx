ee399fa1c43f55f9d16a6737d57049d6c0f9b709
Piotr Gerke <piotrg2003@gmail.com>
2022-10-29 21:44:28 +0200

    higher quality

--- pages/index.tsx
+++ pages/index.tsx
@@ -239,7 +239,7 @@ createGlobalStyle`
 
 const WorldBackground = styled.div`
   width: 100%;
-  height: 108vh;
+  height: 104vh;
   position: absolute;
   @media only screen and (min-width: 768px) {
     height: 110vh;
